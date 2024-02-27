808852bffb5951d891987ae9fc92ad1444b7ab6d
nquinn10 <ntquinn10@gmail.com>
2024-02-27 12:19:11 -0500

    notes added to fetch-promise-results.js

--- src/fetch-promise-results.js
+++ src/fetch-promise-results.js
@@ -2,6 +2,12 @@ const fetchPromise = fetch(
   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
 );
 
+// take fetch response and call JSON
+// JSON gives us promise - executes call async on worker thread
+// jsonPromise.then - logs products
+// similar to callback hell problem
+// json promise depends on fetch.promise completing
+// promise api allows us to chain more elgantly
 fetchPromise.then((response) => {
   const jsonPromise = response.json();
   jsonPromise.then((products) => {
@@ -12,3 +18,5 @@ fetchPromise.then((response) => {
 });
 
 console.log('Fetching products ... ');
+
+// whole script executes, ones response is received, Fetching products... then executes
