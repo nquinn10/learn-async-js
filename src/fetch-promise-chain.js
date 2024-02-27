0c0c9bc8d11596097326f2e18f1671579f2f66b9
nquinn10 <ntquinn10@gmail.com>
2024-02-27 12:22:05 -0500

    notes added to fetch-promise-chain.js

--- src/fetch-promise-chain.js
+++ src/fetch-promise-chain.js
@@ -1,7 +1,15 @@
 const fetchPromise = fetch(
     "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
   );
-  
+
+// chain of then blocks but not nested
+// once fetch is complete, invokes first then block
+// retrieves response from json - async call
+// return occurs in next then block and is processed there
+// chaining then blocks sequentially
+// promise api allows us to execute some piece of code asynchronously and
+// allows us to chain promises, especially if promises depend on each other
+
   fetchPromise
     .then((response) => response.json())
     .then((products) => {
