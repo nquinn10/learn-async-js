a3e909123e682842d66da5cb666f7d5915034ff4
nquinn10 <ntquinn10@gmail.com>
2024-02-27 12:14:15 -0500

    notes added to fetch-promise.js

--- src/fetch-promise.js
+++ src/fetch-promise.js
@@ -3,7 +3,11 @@ const fetchPromise = fetch(
   );
   
   console.log(fetchPromise);
-  
+
+  // when function completes, execute within then block
+  // execute all statements that start after promise
+  // we can define functions to run async by returning a promise or we can use defined apis that return promises
+  // return promise outside of main thread
   fetchPromise.then((response) => {
     console.log(`Received response? ${response.ok? 'yes' : 'no'}`);
     console.log(`Response Status: ${response.status}`);
