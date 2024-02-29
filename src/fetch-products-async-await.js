e1c524406f168bd2157876dc9b75ec15cc921251
nquinn10 <ntquinn10@gmail.com>
2024-02-29 15:04:34 -0500

    notes added to fetch-products-async-await.js

--- src/fetch-products-async-await.js
+++ src/fetch-products-async-await.js
@@ -1,8 +1,9 @@
+// function is either async or it returns a promise
 async function fetchProducts() {
     console.log('Begin fetch ... ');
         // after this line, our function will wait for the `fetch()` call to be settled
         // the `fetch()` call will either return a Response or throw an error
-    const response = await fetch(
+    const response = await fetch( // schedule this on another thread to run async
         "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
     );
     console.log('Fetch done');
@@ -17,9 +18,9 @@ async function fetchProducts() {
   }
   
   async function main() {
-    console.log('Begin main ... ')
+    console.log('Begin main ... ') // starts here, executes synchronously
     try {
-        const productName = await fetchProducts();
+        const productName = await fetchProducts(); // in order to call, needs to be prepended with await
         console.log(productName);
     }
     catch(err) {
